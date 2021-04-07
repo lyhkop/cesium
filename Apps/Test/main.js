264e0e6f9850eda311449c840e3624db70b9047e
lyh <799723343@qq.com>
2021-04-07 15:45:41 +0800

    add pickcoordinate demo page

--- Apps/Test/main.js
+++ Apps/Test/main.js
@@ -1,6 +1,9 @@
+window.CESIUM_BASE_URL = "../../Source/";
 import * as Cesium from "../../Source/Cesium.js";
 
 function main() {
   console.log(Cesium.VERSION);
+
+  let viewer = new Cesium.Viewer("cesiumContainer");
 }
 main();
